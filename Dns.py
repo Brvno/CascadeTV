e61ad94a9698ef8fea0d51390800e8d885388abd
Bruno Ferreira <Brvno@users.noreply.github.com>
2015-04-30 16:22:53 -0300

    Done

--- Dns.py
+++ Dns.py
@@ -1,30 +1,48 @@
 import socket
 
-# UDP server
-UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-UDPSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+class DnsServer(object):
+    def __init__(self):
+        # UDP server
+        self.UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)    
+        self.UDPSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # Listen on port 10000
+        # (to all IP addresses on this system)
+        self.listen_addr = ("",10000)
+        self.stream_lists = {}
 
-# Listen on port 10000
-# (to all IP addresses on this system)
-listen_addr = ("",10000)
-UDPSock.bind(listen_addr)
 
-stream_lists = {}
+    #envia a lista das streams para os viewers
+    def send_list(self, addr):
+        self.selfUDPSock.sendto(str(self.stream_lists), addr)
 
-# Report on all data packets received
-# data -> <tipo>:<nome>
+    #comeca DNS Server
+    def start(self):
+        self.UDPSock.bind(self.listen_addr)
+        while True:
+            data, addr = self.UDPSock.recvfrom(1024)
+            string = data.split(':')
+            print string
+            print data, addr
+            print " --Stream List-- "
+            print self.stream_lists
+            # Report on all data packets received
+            # data -> <tipo>:<nome>
+
+            #Se recebeu uma mensagem de uma stream,adiciona a stream na lista de todas as stream
+            if string[0] == 'stream':
+                self.stream_lists[string[1]] = addr
+            #se recebeu uma mensagem de um viewer ele envia de volta uma lista com todas as streams e o ip dos streamers
+            elif string[0] == 'viewer':
+                self.send_list(addr) 
+            
+    
+
+
+
 print 'CascadeTV DNS online'
-while True:
-        data, addr = UDPSock.recvfrom(1024)
-        string = data.split(':')
-        print string
-        print data, addr
-        print " --Stream List-- "
-	print stream_lists
-	#Se recebeu uma mensagem de uma stream,adiciona a stream na lista de todas as stream
-        if string[0] == 'stream':
-            stream_lists[string[1]] = addr
-	#se recebeu uma mensagem de um viewer ele envia de volta uma lista com todas as streams e o ip dos streamers
-        elif string[0] == 'viewer':
-            UDPSock.sendto(str(stream_lists), addr)
+
+CascadeTvDns = DnsServer()
+CascadeTvDns.start()
+
+    
             
